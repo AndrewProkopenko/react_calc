081c958a00d0b455ef9815d5390e141dbcdd2678
AndrewProkopenko <i.andrewprokopenko@gmail.com>
2020-08-26 15:13:55 +0300

    Updates

---
+++ precache-manifest.01a696c590a2be6fc39c2a185a5fd385.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8402feb19f6490d45f8fcf1370c986",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "31ea9a7e435fa65c72b3",
+    "url": "/react-gh-pages/static/css/main.cfeb1bb7.chunk.css"
+  },
+  {
+    "revision": "ea003352b8f3b4f5fd5b",
+    "url": "/react-gh-pages/static/js/2.5e881f07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.5e881f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ea9a7e435fa65c72b3",
+    "url": "/react-gh-pages/static/js/main.cac62a20.chunk.js"
+  },
+  {
+    "revision": "a6ac14b00d6d9573a4b5",
+    "url": "/react-gh-pages/static/js/runtime-main.70c7d8ed.js"
+  },
+  {
+    "revision": "24cfe579e7c3fe71992f3bd43a167d38",
+    "url": "/react-gh-pages/static/media/fontello.24cfe579.woff2"
+  },
+  {
+    "revision": "6bf044c6f4cc921c248e1532f4d49da6",
+    "url": "/react-gh-pages/static/media/fontello.6bf044c6.svg"
+  },
+  {
+    "revision": "9704a6f0705d43249d5d6022fc8fd6dd",
+    "url": "/react-gh-pages/static/media/fontello.9704a6f0.eot"
+  },
+  {
+    "revision": "a6e2e113a56459bbb76f956261564848",
+    "url": "/react-gh-pages/static/media/fontello.a6e2e113.ttf"
+  },
+  {
+    "revision": "fe02632f151fe7aad3cc86c6d8efbdf1",
+    "url": "/react-gh-pages/static/media/fontello.fe02632f.woff"
+  }
+]);
